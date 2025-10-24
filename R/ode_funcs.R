c6e94e83607459d0efeee7a6e58cc9b16e0d307e
Reis <Daniel.Reis2@va.gov>
2025-10-24 13:11:28 -0600

    Added a test function. Attempted to add .Rhistory to .gitignore

--- R/ode_funcs.R
+++ R/ode_funcs.R
@@ -43,3 +43,8 @@ dHCL <- function(time, states, parms){
     return(list(c(dhdt = dhdt, dndt = dndt, dxdt = dxdt, dydt = dydt, dsleepdt = dsleepdt)))
   })
 }
+
+### TESTING ADDING SOME CODE ###
+testFunc <- function(){
+  NULL
+}
