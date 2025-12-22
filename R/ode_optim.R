32280c1e468944e93ce363963b326c169015b135
Reis <Daniel.Reis2@va.gov>
2025-12-22 16:02:21 -0700

    Added a function and tests to jointly estimate tau and mu parameters. Doesn't seem as accurate and adds slow tests.

--- R/ode_optim.R
+++ R/ode_optim.R
@@ -6,7 +6,7 @@
 #' The difference between timings is calculated as a phase angle.
 #'
 #' @param tau_c Parameter value for tau_c passed by optimize().
-#' @param mid_sleep Observed sleep midpoint timing for calculating residual.
+#' @param sleep_mid Observed sleep midpoint timing for calculating residual.
 #' @param desolve_args List of arguments passed to odeIter().
 #' @param max_iter Max iterations to be passed to odeIter().
 #' @param dur_tol Tolerance allowed for sleep duration (hours) to determine convergence.
@@ -17,7 +17,7 @@
 #' @returns The squared residual between estimated and observed sleep midpoint timing.
 #' @noRd
 #'
-odeOptim_midpoint <- function(tau_c, mid_sleep, desolve_args, max_iter, dur_tol, mid_tol){
+odeOptim_midpoint <- function(tau_c, sleep_mid, desolve_args, max_iter, dur_tol, mid_tol){
 
   ## update tau_c in desolve_args ##
   desolve_args[["parms"]][["tau_c"]] <- tau_c
@@ -33,9 +33,9 @@ odeOptim_midpoint <- function(tau_c, mid_sleep, desolve_args, max_iter, dur_tol,
   } else{
 
     # phase angle - take midpoint of final iteration
-    diff <- clockAngle(ode_res$sleep_sum$sleep_midpoint[nrow(ode_res$sleep_sum)], mid_sleep)
+    diff <- clockAngle(ode_res$sleep_sum$sleep_midpoint[nrow(ode_res$sleep_sum)], sleep_mid)
     diff_squared <- diff^2
-    # print(paste("Values for", tau_c, "are: diff = ", sprintf("%.50f", diff), "and diff^2 = ", diff_squared))
+    # print(paste("Values for", tau_c, "are: diff = ", sprintf("%.5f", diff), "and diff^2 = ", diff_squared))
     return(diff_squared)
   }
 
@@ -56,8 +56,8 @@ odeOptim_midpoint <- function(tau_c, mid_sleep, desolve_args, max_iter, dur_tol,
 #' @returns The squared residual between estimated and observed sleep duration.
 #' @noRd
 #'
-odeOptim_duration <- function(mu, sleep_dur, desolve_args, max_iter = 20,
-                              dur_tol = 1/60, mid_tol = 1/60){
+odeOptim_duration <- function(mu, sleep_dur, desolve_args, max_iter,
+                              dur_tol, mid_tol){
 
   ## update mu in desolve_args ##
   desolve_args[["parms"]][["mu"]] <- mu
@@ -75,12 +75,51 @@ odeOptim_duration <- function(mu, sleep_dur, desolve_args, max_iter = 20,
     # phase angle - take midpoint of final iteration
     diff <- ode_res$sleep_sum$sleep_duration[nrow(ode_res$sleep_sum)] - sleep_dur # difference in sleep durations
     diff_squared <- diff^2 # square of sleep duration difference
-    # print(paste("Values for", mu, "are: diff = ", sprintf("%.50f", diff), "and diff^2 = ", diff_squared))
+    # print(paste("Values for", mu, "are: diff = ", sprintf("%.5f", diff), "and diff^2 = ", diff_squared))
 
     return(diff_squared)
   }
 }
 
+#' Joint optimization of tau and mu
+#'
+#' @param pars Vector of estimates for parameters tau_c and mu
+#' @param sleep_mid Observed sleep midpoint timing for calculating residual.
+#' @param sleep_dur Observed sleep duration for calculating residual.
+#' @param desolve_args List of arguments passed to odeIter().
+#' @param max_iter Max iterations to be passed to odeIter().
+#' @param dur_tol Tolerance allowed for sleep duration (hours) to determine convergence.
+#' Passed to odeIter().
+#' @param mid_tol Tolerance allowed for sleep midpoint (hours) to determine convergence.
+#' Passed to odeIter().
+#'
+#' @returns The sum of squared residuals for sleep midpoint and sleep duration estimates.
+#' @noRd
+#'
+odeOptim_tauAndMu <- function(pars, sleep_mid, sleep_dur, desolve_args,
+                              max_iter, dur_tol, mid_tol){
+
+  ## update tau_c (pars[1]) and mu (pars[2]) in desolve_args ##
+  desolve_args[["parms"]][["tau_c"]] <- pars[1]
+  desolve_args[["parms"]][["mu"]] <- pars[2]
+
+  ## iterate ##
+  ode_res <- odeIter(desolve_args=desolve_args, max_iter = max_iter, dur_tol = dur_tol, mid_tol = mid_tol)
+
+  ## check against observed midsleep time ##
+  if(ode_res$converge == FALSE){
+
+    # return large number if the model did not converge for that value - arbitrary but should be greater than any physiological value
+    return(13^2 + 24^2)
+  } else{
+
+    # return sum of squared residuals #
+    sr_mid <- clockAngle(ode_res$sleep_sum$sleep_midpoint[nrow(ode_res$sleep_sum)], sleep_mid)^2 # squared difference in sleep midpoints
+    sr_dur <- (ode_res$sleep_sum$sleep_duration[nrow(ode_res$sleep_sum)] - sleep_dur)^2 # squared difference in sleep durations
+
+    return(sr_mid+sr_dur)
+  }
+}
 
 
 #' Check if sum of squared residuals is < .03
--- tests/testthat/test-ode_optim.R
+++ tests/testthat/test-ode_optim.R
@@ -42,9 +42,9 @@ test_that("odeOptim_midpoint() returns 13^2 on non-convergence", {
       desolve_list2$forcings <- cbind(times, light2)
 
       # run through optim_midpoint function #
-      res1 <- odeOptim_midpoint(tau_c = 27, mid_sleep = 5.25, desolve_args = desolve_list,
+      res1 <- odeOptim_midpoint(tau_c = 27, sleep_mid = 5.25, desolve_args = desolve_list,
                                 max_iter = 20, dur_tol = 1/60, mid_tol = 1/60) # if testing excessively large tau that won't converge
-      res2 <- odeOptim_midpoint(tau_c = 24.2, mid_sleep = 5.25, desolve_args = desolve_list2,
+      res2 <- odeOptim_midpoint(tau_c = 24.2, sleep_mid = 5.25, desolve_args = desolve_list2,
                                 max_iter = 20, dur_tol = 1/60, mid_tol = 1/60) # if model won't converge because of insufficient light
 
 
@@ -109,7 +109,7 @@ test_that("odeOptim_midpoint() works", {
       syn_mid <- syn_sol$sleep_sum$sleep_midpoint[nrow(syn_sol$sleep_sum)]
 
       # optimize - optimize if for 1D optimization.
-      opt_midpoint <- optimize(f = odeOptim_midpoint, interval = c(22, 26), mid_sleep = syn_mid,
+      opt_midpoint <- optimize(f = odeOptim_midpoint, interval = c(22, 26), sleep_mid = syn_mid,
                       desolve_args = desolve_list, max_iter = 20,
                       dur_tol = 1/60, mid_tol = 1/60)
 
@@ -130,7 +130,7 @@ test_that("odeOptim_midpoint() works", {
       syn_mid2 <- syn_sol2$sleep_sum$sleep_midpoint[nrow(syn_sol2$sleep_sum)]
 
       # optimize
-      opt_midpoint2 <- optimize(f = odeOptim_midpoint, interval = c(22, 26), mid_sleep = syn_mid2,
+      opt_midpoint2 <- optimize(f = odeOptim_midpoint, interval = c(22, 26), sleep_mid = syn_mid2,
                                desolve_args = desolve_list2, max_iter = 20,
                                dur_tol = 1/60, mid_tol = 1/60)
 
@@ -318,6 +318,199 @@ test_that("odeOptim_duration() works", {
   expect_equal(opt_duration$objective^2 < .03, TRUE)
   expect_equal(opt_duration2$objective^2 < .03, TRUE)
 
+})
+
+
+# Joint parameter optimization --------------------------------------------
+
+test_that("odeOptim_tauAndMu() returns 13^2 + 24^2 upon non-convergence", {
+
+
+  ## using tryCatch() so that dyn.load is always removed,
+  ## even if the test errors out
+  tryCatch(
+    {
+      ## check if .dll is loaded, load if needed (will unload after test)
+      ## TODO - is there a better way of loading c code functions for testing?
+      if(!"rHCL" %in% names(getLoadedDLLs())){
+        # using here package to find root of rstudio project directory b/c
+        # when running test suite the working directory switches to test folder
+        dyn.load(paste(here::here(), "src/rHCL.dll", sep = "/"))
+      }
+
+      ## set up times and light entrainment profile
+      times <- seq(0, 24*30, by = .2) # 12-minute intervals
+      light <- lightCycle(times) # default light profile
+      light2 <- rep(0, length(times))
+
+      # standard set up
+      desolve_list <- list(
+        y = c(h = 13.15, n = .152, x = -0.966, y = -0.558, S = 0),
+        times = times,
+        func = "derivsc_p",
+        parms = unlist(hclParms()),
+        dllname = "rHCL",
+        initforc = "forcc_p",
+        forcings = cbind(times, light),
+        fcontrol = list(method="linear", rule=2, f=0),
+        initfunc = "parmsc_p",
+        nout = 0,
+        events = list(func="eventc_p", root=TRUE),
+        rootfun = "rootc_p",
+        nroot = 1
+      )
+
+      # no light
+      desolve_list2 <- desolve_list
+      desolve_list2$forcings <- cbind(times, light2)
+
+      # run through optim_tauAndMu function #
+      res1 <- odeOptim_tauAndMu(pars=c(24.2, 1000), sleep_mid = 3.5, sleep_dur = 7.4, desolve_args = desolve_list,
+                                max_iter = 20, dur_tol = 1/60, mid_tol = 1/60) # if testing excessively large mu that won't converge
+      res2 <- odeOptim_tauAndMu(pars=c(35, 17), sleep_mid = 3.5, sleep_dur = 7.4, desolve_args = desolve_list,
+                                max_iter = 20, dur_tol = 1/60, mid_tol = 1/60) # if testing excessively large tau that won't converge
+      res3 <- odeOptim_tauAndMu(pars=c(24.2, 17.87), sleep_dur = 7.4, desolve_args = desolve_list2,
+                                max_iter = 20, dur_tol = 1/60, mid_tol = 1/60) # if model won't converge because of insufficient light
+
+
+    },
+    finally = {
+      # unload .dll
+      if("rHCL" %in% names(getLoadedDLLs())){
+        dyn.unload(paste(here::here(), "src/rHCL.dll", sep = "/"))
+      }
+    }
+  )
+
+  expect_equal(res1, 13^2 + 24^2)
+  expect_equal(res2, 13^2 + 24^2)
+  expect_equal(res3, 13^2 + 24^2)
+})
+
+
+
+test_that("odeOptim_tauAndMu() works", {
+
+
+  ## using tryCatch() so that dyn.load is always removed,
+  ## even if the test errors out
+  tryCatch(
+    {
+      ## check if .dll is loaded, load if needed (will unload after test)
+      ## TODO - is there a better way of loading c code functions for testing?
+      if(!"rHCL" %in% names(getLoadedDLLs())){
+        # using here package to find root of rstudio project directory b/c
+        # when running test suite the working directory switches to test folder
+        dyn.load(paste(here::here(), "src/rHCL.dll", sep = "/"))
+      }
+
+      ## set up times and light entrainment profile
+      times <- seq(0, 24*7, by = .2) # 12-minute intervals
+      light <- lightCycle(times, l2=10) # generate standard light profile
+
+      ## set up a synthetic sleep wake cycle ##
+
+      # create a list for deSolve::ode arguments #
+      desolve_list <- list(
+        # initial values, should correspond to a 4:24 am ymin time and ~7.4 hours of nightly sleep
+        y = c(h = 13.15, n = .152, x = -0.966, y = -0.558, S = 0),
+        times = times,
+        func = "derivsc_p",
+        parms = unlist(hclParms(tau_c = 24.4, mu = 16.5)), # set desired mu value
+        dllname = "rHCL",
+        initforc = "forcc_p",
+        forcings = cbind(times, light),
+        fcontrol = list(method = "linear", rule=2, f=0),
+        initfunc = "parmsc_p",
+        nout = 0,
+        events = list(func = "eventc_p", root = TRUE),
+        rootfun = "rootc_p",
+        nroot = 1
+      )
+
+      # extract sleep outcomes of synthetic data #
+      syn_sol <- odeIter(desolve_args = desolve_list,
+                         max_iter = 20, dur_tol = 1/60, mid_tol = 1/60)
+      if(syn_sol$converge==FALSE){
+        stop("Synthetic syn_sol data did not converge")
+      }
+
+      syn_mid <- syn_sol$sleep_sum$sleep_midpoint[nrow(syn_sol$sleep_sum)]
+      syn_dur <- syn_sol$sleep_sum$sleep_duration[nrow(syn_sol$sleep_sum)]
+
+      # optimize for multiple parameters
+      opt_optim1 <- optim(par = c(24.2, 17), fn = odeOptim_tauAndMu,
+                          sleep_mid = syn_mid, sleep_dur = syn_dur,
+                          desolve_args = desolve_list, max_iter = 20,
+                          dur_tol = 1/60, mid_tol = 1/60,
+                          control = list(maxit = 1000))
+
+      # # try bobyqa for gradient free optimization
+      # min_mu <- desolve_list[["parms"]][["Hzero"]] + desolve_list[["parms"]][["ca_par"]] + desolve_list[["parms"]][["delta"]]*.05 # constrain mu to be greater than this
+      #
+      # opt_bobyqa1 <- nloptr::bobyqa(x0 = c(24.2, 17.87), fn = odeOptim_tauAndMu,
+      #                               lower = c(22, min_mu),
+      #                               upper = c(26, 30),
+      #                               sleep_mid = syn_mid, sleep_dur = syn_dur,
+      #                               desolve_args = desolve_list, max_iter = 20,
+      #                               dur_tol = 1/60, mid_tol = 1/60,
+      #                               control = list(xtol_rel = 1e-8))
+
+
+      ## alternative mu value ##
+      desolve_list2 <- desolve_list
+      desolve_list2[["forcings"]] <- cbind(times, lightCycle(times, l2=10)) # decrease night light exposure to aid more extreme entrainment
+      desolve_list2$parms[["tau_c"]] <- 23.8
+      desolve_list2$parms[["mu"]] <- 19.9 # alternative mu
+
+
+      # generate synthetic results and extract sleep midpoint
+      syn_sol2 <- odeIter(desolve_args = desolve_list2,
+                          max_iter = 20, dur_tol = 1/60, mid_tol = 1/60)
+      # check for convergence on synthetic data
+      if(syn_sol2$converge==FALSE){
+        stop("Synthetic syn_sol2 data did not converge")
+      }
+      syn_mid2 <- syn_sol2$sleep_sum$sleep_midpoint[nrow(syn_sol2$sleep_sum)]
+      syn_dur2 <- syn_sol2$sleep_sum$sleep_duration[nrow(syn_sol2$sleep_sum)]
+
+      # optimize
+      opt_optim2 <- optim(par = c(24.2, 18.5), fn = odeOptim_tauAndMu,
+                          sleep_mid = syn_mid2, sleep_dur = syn_dur2,
+                          desolve_args = desolve_list2, max_iter = 20,
+                          dur_tol = 1/60, mid_tol = 1/60)
+#
+#       # try bobyqa for gradient free optimization
+#       min_mu2 <- desolve_list2[["parms"]][["Hzero"]] + desolve_list2[["parms"]][["ca_par"]] + desolve_list2[["parms"]][["delta"]]*.05 # constrain mu to be greater than this
+#
+#       opt_bobyqa2 <- nloptr::bobyqa(x0 = c(24.2, 18.5), fn = odeOptim_tauAndMu,
+#                                     lower = c(22, min_mu2),
+#                                     upper = c(26, 30),
+#                                     sleep_mid = syn_mid2, sleep_dur = syn_dur2,
+#                                     desolve_args = desolve_list2, max_iter = 20,
+#                                     dur_tol = 1/60, mid_tol = 1/60,
+#                                     control = list(xtol_rel = 1e-8))
+
+    },
+    finally = {
+
+      # unload .dll
+      if("rHCL" %in% names(getLoadedDLLs())){
+        dyn.unload(paste(here::here(), "src/rHCL.dll", sep = "/"))
+      }
+    }
+  )
+
+  # ## check if optimizer close to actual values - Not the most accurate ##
+  expect_equal(abs(opt_optim1$par[1] - 24.4) < .15, TRUE)
+  expect_equal(abs(opt_optim1$par[2] - 16.5) < .2, TRUE)
+
+  expect_equal(abs(opt_optim2$par[1] - 23.8) < .15, TRUE)
+  expect_equal(abs(opt_optim2$par[2] - 19.9) < .15, TRUE)
+
+  ## check that deviances (squared) for minimum values are below .03 ##
+  expect_equal(opt_optim1$value < .03, TRUE)
+  expect_equal(opt_optim2$value < .03, TRUE)
 })
 
 
